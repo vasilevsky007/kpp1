47350335b5449d54e9bfc00a3fd35194301d63de
vasilevsky007 <alex.vasilevski007@gmail.com>
2022-03-21 10:36:47 +0300

    Initial commit

--- src/main/java/com/kpp/demo1/Demo1Controller.java
+++ src/main/java/com/kpp/demo1/Demo1Controller.java
@@ -12,7 +12,7 @@ public class Demo1Controller {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/lab1")
-    public ReturnValue greeting(@RequestParam(value = "number", defaultValue = "100") String snumber, @RequestParam(value = "calculation", defaultValue = "+1") String calculation) {
+    public ReturnValue rval(@RequestParam(value = "number", defaultValue = "100") String snumber, @RequestParam(value = "calculation", defaultValue = "-1") String calculation) {
 
         return new ReturnValue(counter.incrementAndGet(),Long.parseLong(snumber), calculation);
     }
